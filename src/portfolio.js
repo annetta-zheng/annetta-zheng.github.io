d1ab14c82d3a1e66ae35a50bfd7a4cce95ab2153
Annetta <67286396+annetta-zheng@users.noreply.github.com>
2023-11-07 22:14:37 -0500

    publish 1107-2

--- src/portfolio.js
+++ src/portfolio.js
@@ -52,15 +52,15 @@ const socialMediaLinks = {
 const skillsSection = {
   title: "What I do",
   subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
-  skills: [
-    emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
-    ),
-    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
-    emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
-    )
-  ],
+  // skills: [
+  //   emoji(
+  //     "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+  //   ),
+  //   emoji("⚡ Natrual Language Processing and Machine Learning"),
+  //   // emoji(
+  //   //   "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+  //   // )
+  // ],
 
   /* Make Sure to include correct Font Awesome Classname to view your icon
 https://fontawesome.com/icons?d=gallery */
